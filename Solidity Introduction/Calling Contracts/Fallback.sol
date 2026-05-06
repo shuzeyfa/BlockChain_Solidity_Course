56b98fdf0e04e588d0061438e2a9d1abacc76a69
huzeyfa <shuzeyfa4@gmail.com>
2026-05-06 17:27:45 +0300

    feat: add calling contracts implementation and examples

--- Solidity Introduction/Calling Contracts/Fallback.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract Sidekick {
-    function makeContact(address hero) external {
-        (bool success, ) = hero.call("0xdeadbeef");
-        require(success);
-    }
-}
