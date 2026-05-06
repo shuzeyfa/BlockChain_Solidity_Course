65e467bda946ca74a3f75a7c9b49f33641de4e03
Tesfaye Zeleke <132948928+abizeleke@users.noreply.github.com>
2026-05-06 20:43:51 +0300

    Implement filterEven function to store even numbers

---
+++ Reference Types /Arrays /Filter to storage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Contract {
+    uint[] public evenNumbers;
+
+    function filterEven(uint[] calldata numbers) external {
+        delete evenNumbers;
+
+        for (uint i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                evenNumbers.push(numbers[i]);
+            }
+        }
+    }
+}
