f4c4bcc0626184861b30bde345704c2a67d9aed4
huzeyfa <shuzeyfa4@gmail.com>
2026-05-06 17:30:01 +0300

    feat: add calling contracts and Sending Ether implementation and examples

---
+++ Address Interaction/Calling Contracts/Fallback.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Sidekick {
+    function makeContact(address hero) external {
+        (bool success, ) = hero.call("0xdeadbeef");
+        require(success);
+    }
+}
